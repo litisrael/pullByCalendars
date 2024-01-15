2c51dcf73b1b01101387a76548260687dc4b65fe
litisrael <isroellit@gmail.com>
2024-01-15 11:43:44 +0200

    async dates

--- src/components/claendar/Calendarday.tsx
+++ src/components/claendar/Calendarday.tsx
@@ -3,12 +3,14 @@ import { DatePicker } from '@mantine/dates';
 
 interface CalendarDayPickerProps {
     minDate?: Date; // Añadir ? para que la prop sea opcional
+    maxDate?: Date
 }
 
-export const CalendarDayPicker: React.FC<CalendarDayPickerProps> = ({ minDate  }) => {
+export const CalendarDayPicker: React.FC<CalendarDayPickerProps> = ({ minDate, maxDate  }) => {
   const [value, setValue] = useState<Date | null>(null);
 
   return <DatePicker
   minDate={minDate}
-  maxDate={new Date() } value={value} onChange={setValue} />;
+  defaultDate={maxDate}
+  maxDate={maxDate } value={value} onChange={setValue} />;
 };
--- src/components/claendar/selectCalendar/SelectCalendar.tsx
+++ src/components/claendar/selectCalendar/SelectCalendar.tsx
@@ -1,43 +1,71 @@
-import { Select, Group } from '@mantine/core';
-import { useState } from 'react';
-import { DatePicker,MonthPicker } from '@mantine/dates';
-import { CalendarDayPicker} from "../Calendarday";
+import { useEffect, useState } from 'react';
+import { Select, Group, Alert } from '@mantine/core';
+import { DatePicker, MonthPicker } from '@mantine/dates';
+import { CalendarDayPicker } from '../Calendarday';
 import '@mantine/dates/styles.css';
-
-
-const componentsMap: Record<string, React.ReactNode> = {
- 
-  'One day': <CalendarDayPicker minDate={minDate} />,
-  // 'From day, To day': null,
-'One month': <MonthPicker/>
-//   'All Days': null,
-};
+import { supabase } from '../../../conection/client.js';
 
 export function SelectOptionComponent() {
+ 
   const [selectedValue, setSelectedValue] = useState<string | null>('One day');
+  const [minDate, setMinDate] = useState<Date | undefined>(undefined)
+  const [maxDate, setMaxDate] = useState<Date |  undefined>(undefined)
+  const [datesLoaded, setDatesLoaded] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabase.rpc('maxymindates');
+      const firstData = data && data[0];
+
+      if (!firstData) {
+        console.log(error);
+        
+        throw new Error('Error al conectar con la base de datos');
+      }
+
+      const { mindate, maxdate } = firstData;
+      console.log('Fechas min:', mindate, 'Fechas max:', maxdate);
+      setMinDate(new Date(mindate));
+      setMaxDate(new Date(maxdate));
+      setDatesLoaded(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!datesLoaded) {
+    return <p>Cargando fechas...</p>;
+  }
+  const componentsMap: Record<string, React.ReactNode> = {
+    'One day': <CalendarDayPicker minDate={minDate} maxDate={maxDate} />,
+    'One month': <MonthPicker />,
+  };
+
   const handleSelectChange = (value: string | null) => {
     console.log('valor', value);
-
     setSelectedValue(value);
   };
+
   return (
     <>
-    <Group justify='center'>
-      <Select
-        label="Input label"
-        description="Input description"
-        placeholder="Select placeholder"
-        defaultValue="One day"
-        allowDeselect={false}
-        onChange={handleSelectChange}
-        data={['One day', 'From day, To day', 'One month', 'All Days']}
-        value={selectedValue}
+      <Group justify="center">
+        <Select
+          label="Input label"
+          description="Input description"
+          placeholder="Select placeholder"
+          defaultValue="One day"
+          allowDeselect={false}
+          onChange={handleSelectChange}
+          data={['One day', 'One month']}
+          value={selectedValue}
         />
-    </Group>
-    <Group justify='center'>
-
-        {componentsMap[selectedValue ]}
-    </Group>
-        </>
+      </Group>
+      <Group justify="center">{componentsMap[selectedValue]}</Group>
+    </>
   );
 }
+
